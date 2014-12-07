e51ad7676e14882755c0f96b45ab008195cfe116
inliner <andrey.artemenko@gmail.com>
2014-12-07 16:20:21 +0200

    t3

--- js/main.js
+++ js/main.js
@@ -4,5 +4,9 @@ $(document).ready(function(){
 	auto: true,
 	pause: 5000
   });
+
+  $(".details_btn").click(function () {
+  	$(this).closest(".room_item").find(".room_details").toggle();
+  });
   
 });
